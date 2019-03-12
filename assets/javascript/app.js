aa6a979ad01a1b63422ec073eef1591007e842de
bryvl <bryan.val93@gmail.com>
2019-03-12 00:21:40 -0400

    added timer breaker and attempt at loop for checking checked radio buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
     var count = 0;
     var time = 120;
-    var intervalID = setInterval(timer, 100);
+    var intervalID = setInterval(timer, 50);
     var correctCount = 0;
     var wrongCount = 0;
+    var answered = 0;
+    var questions = 8;
+    var radios = $('input:radio');
 
 
 
@@ -11,7 +14,7 @@ $(document).ready(function() {
         $("#timer").html("Timer: " + time + " seconds left");
         var resultsDiv = $('<div class="results">');
         var correctDiv = $('<p id="correct">');
-        var wrongDiv = $('<p id="correct">');
+        var wrongDiv = $('<p id="wrong">');
 
 
         if (time === 0) {
@@ -20,9 +23,21 @@ $(document).ready(function() {
             time = 5;
             resultsDiv.text("Time's Up!");
             $('#timer').html(resultsDiv);
+            var inputs = $('input');
+            for (var i = 0; i < radios.length; i++) {
+                if(radios.is(':checked') === true) {
+                    answered++;
+                }
+            }
+            console.log(answered);
         }
         time--;
     };
+    
+    $('#finishQuiz').on('click', function(){
+        clearInterval(intervalID);
+        $('#timer').html("All done!");
+    });
 
 
 });
